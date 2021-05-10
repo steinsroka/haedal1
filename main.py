913fbb5af340c71a89cd74efd1560aff835bbd2a
steinsroka <62500287+steinsroka@users.noreply.github.com>
2021-05-10 15:43:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+class Haedal:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, sname, sid):
+        self.sname = sname
+        self.sid = sid
+
+    def info(self):
+        print(f'이름 : {self.sname}')
+        print(f'학번 : {self.sid}')
+        print()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+std1 = Haedal('김해규', 2017111111)
+std2 = Haedal('이해규', 2018111111)
+std3 = Haedal('박해규', 2019111111)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+std1.info()
+std2.info()
+std3.info()
